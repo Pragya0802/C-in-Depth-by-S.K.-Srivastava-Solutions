614cbdab418cca43cc8c02a905ed2485612c3a3b
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-19 10:58:04 +0530

    Create 42.c

---
+++ 6. Functions/42.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int prodDigits(int);       //Function Prototype
+void main()
+{
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);        //taking input
+    printf("Product of the digits = %d",prodDigits(n));     //Function call and printing the output
+}
+int prodDigits(int n)       
+{
+    int r,pod=1;
+    while(n!=0)
+    {
+        r=n%10;       //extracting the last digit
+        pod=pod*r;    //multiplying the las digit
+        n=n/10;       //making changes in original number
+    }
+    return pod;       // returning product of digits
+}

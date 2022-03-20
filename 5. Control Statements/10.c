03d430f08845b07c48507d69b39868b3d8e801a3
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 10:58:51 +0530

    Create 10.c

---
+++ 5. Control Statements/10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k,l,m,z;
+    printf("Enter i");
+    scanf("%d",&i);
+    printf("Enter j");
+    scanf("%d",&j);
+    printf("Enter k");
+    scanf("%d",&k);
+    printf("Enter l");
+    scanf("%d",&l);
+    printf("Enter m");
+    scanf("%d",&m);
+    if(i==3 && j==k && l<m)
+        printf("100");
+}

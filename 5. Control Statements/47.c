54fbdd22d895293ff2045894e920760efcd83bf6
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 08:33:23 +0530

    Update 47.c

--- 5. Control Statements/47.c
+++ 5. Control Statements/47.c
@@ -1,14 +1,15 @@
+/* Write a program to input 10 numbers and find the largest number. */
 #include<stdio.h>
 void main()
 {
-    int i,a[10],max=0;
+    int i,a[10],max=0;                    //initializing array of 10 elements
     printf("Enter 10 number");
     for(i=0;i<10;i++)
-        scanf("%d",&a[i]);
-    for(i=0;i<10;i++)
+        scanf("%d",&a[i]);                 //taking input
+    for(i=0;i<10;i++)                      //loop for finding maximum number
     {
         if(a[i]>max)
              max=a[i];
     }
-    printf("max = %d",max);
+    printf("max = %d",max);                 //printing max elements
 }

e0222ae006a3b605e670977766f7a0c5066fb8af
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 08:52:03 +0530

    Update 53.c

--- 5. Control Statements/53.c
+++ 5. Control Statements/53.c
@@ -1,17 +1,18 @@
+/* Write a program to input a number and count the digits in it. Use while loop and the program should work correctly for 0 also. */
 #include<stdio.h>
 void main()
 {
-    int n,c=0,s,r;
+    int n,c=0,s,r;                                  //initializing variable
     printf("Enter the number");
-    scanf("%d",&n);
-    r=n;
-    if(n==0)
+    scanf("%d",&n);                                 //taking input
+    r=n;                                            //storing original number to a new variable
+    if(n==0)                                
         printf("Total number of digits in %d = 1",r);
     else 
     {   while(n!=0)
        {
-            n=n/10;
-            c=c+1;
+            n=n/10;                                     //extracing last digit
+            c=c+1;                                      //flag variable
         }
          printf("Total number of digits in %d = %d",r,c);
     }

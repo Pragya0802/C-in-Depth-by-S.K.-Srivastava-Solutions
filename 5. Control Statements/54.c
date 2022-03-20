963a5396499e5615702c1c45ddb074be6e96d3d2
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 08:58:07 +0530

    Update 54.c

--- 5. Control Statements/54.c
+++ 5. Control Statements/54.c
@@ -1,19 +1,19 @@
+/* Input a number and a digit, and count the number of times the digit occurs in the number. */
 #include<stdio.h>
 void main()
 {
-    int n,c=0,s,r,t;
-    printf("Enter the number");
-    scanf("%d",&n);
+    int n,c=0,s,r,t;                                //initializig variables
+    printf("Enter the number");             
+    scanf("%d",&n);                                 //taking input for number
     printf("Enter a digit to check its frequency");
-    scanf("%d",&r);
-    t=n;
+    scanf("%d",&r);                                  //taking input for digit
+    t=n;                                            //storing variable for future use
     while (n!=0)
     {
-        /* code */
-        s=n%10;
+        s=n%10;                                      //extracting last digit
         if(r==s)
             c++;
-        n=n/10;
+        n=n/10;                                        //making changes in original number
     } 
     printf("%d is present %d times in %d",r,c,t);
 }

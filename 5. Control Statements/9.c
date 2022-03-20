7fb43cb39a36578ed2d5c83581e6c5b33d972a69
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 10:55:37 +0530

    Create 9.c

---
+++ 5. Control Statements/9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int x,a,b;
+  printf("enter x ");
+  scanf("%d",&x);
+  printf("Enter a");
+  scanf("%d",&a);
+  printf("Enter b");
+  scanf("%d",&b);
+  if(x+=a<b)
+      printf("%d",-x);
+  else
+      printf("100");
+}

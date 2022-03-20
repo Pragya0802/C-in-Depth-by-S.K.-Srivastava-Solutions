1625ed5bbdc2703d1ded1f0ae5c1e504698b62b8
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 08:59:35 +0530

    Update 55.c

--- 5. Control Statements/55.c
+++ 5. Control Statements/55.c
@@ -1,11 +1,12 @@
+/* Write a program to accept any number n and print the sum of square of all numbers from I to n. */ 
 #include<stdio.h>
 void main()
 {
     int n,i;
     printf("Enter a limit");
-    scanf("%d",&n);
+    scanf("%d",&n);                      //taking input for n
     for(i=1;i<=n;i++)
     {
-        printf("%d -> %d\n",i,i*i);
+        printf("Square of %d -> %d\n",i,i*i);        //printing the square
     }
 }

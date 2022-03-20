f4a9524aed7f1e6501816966beaaba82cd3efb13
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 08:40:51 +0530

    Update 50.c

--- 5. Control Statements/50.c
+++ 5. Control Statements/50.c
@@ -1,15 +1,16 @@
+/* Write a program to enter a number and find the reverse of that number. Also display the double of the reverse */
 #include<stdio.h>
 void main()
 {
-    int n,r,s;
-    printf("Enter a number : ");
-    scanf("%d",&n);
-    while(n>0)
+    int n,r,s;                                  //initializing variable
+    printf("Enter a number : ");      
+    scanf("%d",&n);                             //taking input
+    while(n>0)                                   
     {
-        r=n%10;
-        s=s*10+r;
-        n=n/10;
+        r=n%10;                                  //extracting last digit
+        s=s*10+r;                                //adding last digit to a new variable
+        n=n/10;                                  // making changes in original number
     }
-    printf("reversed number = %d\n",s);
-    printf("Double of a reverse numbre = %d",2*s);
+    printf("reversed number = %d\n",s);           //printing the reverse number
+    printf("Double of a reverse numbre = %d",2*s); // printing te double of reverse number
 }

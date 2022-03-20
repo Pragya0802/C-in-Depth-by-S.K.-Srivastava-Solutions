ebc45f38dff579fc410126b1e86d3886a941cf89
Pragya0802 <97036938+Pragya0802@users.noreply.github.com>
2022-03-20 12:09:49 +0530

    Update 59i.c

--- 5. Control Statements/59i.c
+++ 5. Control Statements/59i.c
@@ -1,13 +1,16 @@
+/* Find the sum of these series up to n terms where x is an integer entered by user. 
+1 + 11 + 111 + 1111 + ..........  */ 
 #include<stdio.h>
 #include<math.h>
 void main()
 {
-    int i,n,s=0,f=0;
+    int i,n,s=0,f=0;                           //initializing variable
     printf("Enter no. of terms you want to add : ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {    s=s*10+1;
-         f=f+s;
+    scanf("%d",&n);                            //taking input
+    for(i=0;i<n;i++)                           //loop for addition
+    {  
+        s=s*10+1;
+        f=f+s;
     }
     printf("%d",f);
 }
